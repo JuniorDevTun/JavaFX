474f7b7b94a06a6412b00df559786bf80797c047
aymkh <aymen4real@gmail.com>
2020-12-13 00:47:30 +0100

    Scene

---
+++ 02 Scene/Main.java
@@ -0,0 +1,23 @@
+package application;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	@Override
+	public void start(Stage s) {
+		s.setTitle("Junior Stage");
+		s.setHeight(300);
+		s.setWidth(600);		
+		
+        // Layout manager used by the Scene
+		VBox root = new VBox();
+        // Creating a new Scene
+		Scene sc = new Scene(root);
+
+        // Assigning the scene to the Stage
+		s.setScene(sc);
+		s.show();		
+	}	
+	public static void main(String[] args) { launch(args); }
+}
