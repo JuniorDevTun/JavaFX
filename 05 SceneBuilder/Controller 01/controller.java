243b01deef322b8972ba76ed347df3a5bf45fe69
aymkh <aymen4real@gmail.com>
2021-01-01 16:16:48 +0100

    Controlelr 01

---
+++ 05 SceneBuilder/Controller 01/controller.java
@@ -0,0 +1,11 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class controller {
+	
+	public void myMethod() {
+		System.out.prinln("Hi there");
+	}
+}
